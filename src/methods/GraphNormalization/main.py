ee2a1d3ed27a2095c96e00d4fb476f420df5d674
Xenerotos <tim.stohn@yahoo.com>
2021-06-02 17:28:39 +0200

    - added threshold correlation parameter: easily call Graph by zero correlation: which is normalization by genes that are not DE expressed (those having slight DE bahvior after libSizeNorm are excluded by nature of Algo)

--- src/methods/GraphNormalization/main.py
+++ src/methods/GraphNormalization/main.py
@@ -9,25 +9,24 @@ def read_data(data_file):
     data = pd.read_csv(data_file, sep='\t')
     return(data)
 
-def normalize_data(dataset):
+def normalize_data(dataset, correlation):
     corr_method = "spearman"
-    corr_threshold=0.85
+    corr_threshold=correlation
     G = NormalizationGraph(dataset, corr_method, corr_threshold)
     p_val=0.05
     cohend_val=0.5
     take_log=False
-    G.remove_batch_effect()
-    G.normalize_by_library_size()
-    G.get_normalized_score(p_val, cohend_val, take_log)
-    return(G.data)
+    normalized_score_table = G.get_normalized_score(p_val, cohend_val, take_log)
+    batch_eff_removed_table = G.remove_batch_effect(normalized_score_table)
+    return(batch_eff_removed_table)
 
 def ensure_dir(file_path):
     if not os.path.exists(file_path):
         makedirs(file_path)
 
-def run_normalization_on_dataset(data_dir, output_dir = ""):    
+def run_normalization_on_dataset(data_dir, correlation, output_dir = ""):    
     data = read_data(data_dir)
-    result = normalize_data(data)
+    result = normalize_data(data, correlation)
     if(output_dir):
         ensure_dir(output_dir)
         result.to_csv(output_dir + "/GraphNormalized.tsv", sep='\t')
@@ -35,10 +34,14 @@ def run_normalization_on_dataset(data_dir, output_dir = ""):
         result.to_csv(os.path.dirname(data_dir) + "/GraphNormalized.tsv", sep='\t')
 
 def main():
-    if(len(sys.argv) != 2):
+    if(len(sys.argv) != 3 and len(sys.argv) != 2):
         print("ERROR: use script with <python3 graphNormalization.py [directory of datasets]>\n")
         exit(1)
     data_dir = sys.argv[1]
+    if(len(sys.argv) == 3):
+        correlation = float(sys.argv[2])
+    else:
+        correlation = 0.7
     dataset_dir = "./bin/FILTERED_DATASETS"
     if(data_dir == "ALL"):
         #store all not ini files in list (store full path)
@@ -52,9 +55,9 @@ def main():
         for dataset in dataset_list:
             output_dir = "bin/NORMALIZED_DATASETS/" + os.path.basename(os.path.splitext(dataset)[0])
             print("Running Graph normalization on: " + dataset)
-            run_normalization_on_dataset(dataset, output_dir)
+            run_normalization_on_dataset(dataset, correlation, output_dir)
     else:
-        run_normalization_on_dataset(data_dir)
+        run_normalization_on_dataset(data_dir, correlation)
 
 if __name__ == '__main__':
     main()
