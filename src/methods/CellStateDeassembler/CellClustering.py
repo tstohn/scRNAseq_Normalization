df89bcf826c2567b2ea48573331f9124614aef50
Xenerotos <tim.stohn@yahoo.com>
2021-06-17 17:33:51 +0200

    minor things...

--- src/methods/CellStateDeassembler/CellClustering.py
+++ src/methods/CellStateDeassembler/CellClustering.py
@@ -2,7 +2,14 @@ class Cluster:
     def __init__(self, data):
         self.data = data
 
+#represent cells by a function of cell state (cell stae is a continuous space)
+#model this space as a manifold, create a manufold for each treatment and make a mapping of 
+#a manifold distriution function describing all possible cell states to this function for a different treatment
 
+#put those mappings into a auto-encoder to elarn the hidden continuous space of cell state points for different treatments
 class CellClustering:
     def __init__(self, data):
         self.data = data
+
+    def meld_algorithm(self):
+        
