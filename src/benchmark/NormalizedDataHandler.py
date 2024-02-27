- fixed calculation of n/p from mu/size  - calcualting TPR/FPR values for correlations

src/benchmark/NormalizedDataHandler.py
@@ -43,7 +43,10 @@ class NormalizedDataHandler:
 
     def __init_classification(self, data_name, classification_dict):
         data = self.data[data_name]
-        groundtruth = None
+        
+        #dictionary where key is the dataset (Groundtruth, TMM, CLR_COMPOSITIONS,..) and value a dataframe of corelations (see ab_spearman_correlation)
+        #dataframe has form index, SPvalues, Pvalues, AB1, AB2, cluster where index is of form AB1_AB2 
+        self.correlations = {}
 
         #organize data
         d_pivot=data.pivot(index='sample_id', columns='ab_id', values='ab_count_normalized')
@@ -126,10 +129,13 @@ class NormalizedDataHandler:
         self.sp_results.write("NORMALIZATION_METHOD" + "\t" + "SPEARMAN_CORRELATION_MEAN" + "\t" + "SPEARMAN_PVALUE_MEAN" + "\n")
 
         self.rmsd_sp_results = open("bin/BENCHMARKED_DATASETS/"+folder_name+"/Results/spearmanRMSD.tsv", "w+")
-        self.rmsd_sp_results.write("NORMALIZATION_METHOD" + "\t" + "SPEARMAN_CORRELATIONS_RMSD" + "\n")
+        self.rmsd_sp_results.write("CLUSTER_ID" + "\t" + "NORMALIZATION_METHOD" + "\t" + "SPEARMAN_CORRELATIONS_RMSD" + "\n")
 
         self.abCorr = open("bin/BENCHMARKED_DATASETS/"+folder_name+"/Results/ABSpearmanCoeff.tsv", "w+")
-        self.abCorr.write("NORMALIZATION_METHOD" + "\t" + "AB1" + "\t" + "AB2" + "\t" + "GroundtruthCorr" "\t" + "NormCorr\n")
+        self.abCorr.write("CLUSTER_ID" + "\t" + "NORMALIZATION_METHOD" + "\t" + "AB1" + "\t" + "AB2" + "\t" + "GroundtruthCorr" "\t" + "NormCorr\n")
+
+        self.corrDetection = open("bin/BENCHMARKED_DATASETS/"+folder_name+"/Results/PercentageCorrelationDetection.tsv", "w+")
+        self.corrDetection.write("cluster"+ "\t" + "norm"+ "\t" + "minCorr"+ "\t" + "TP"+ "\t" + "TN"+ "\t" + "FP"+ "\t" + "FN" + "\n")
 
         #header is written when stored
         self.knnOverlapFilePath = "bin/BENCHMARKED_DATASETS/"+folder_name+"/Results/knnOverlap.tsv"
@@ -355,6 +361,31 @@ class NormalizedDataHandler:
             graph.render(data_name + "decision_tree_graphivz")
 
     def __calculate_all_spearman_correlations(self, data):
+        
+        data_subset = data.loc[:,['sample_id','ab_id', 'ab_count_normalized']]
+        d_pivot=data_subset.pivot(index = "sample_id", columns='ab_id', values='ab_count_normalized')
+        columns = d_pivot.columns.tolist()
+
+        #dataframe to fill with spearkman values
+        column_names = ["index", "SPvalues", "Pvalues", "AB1", "AB2"]
+        correlations = pd.DataFrame(columns = column_names)
+        for col_a, col_b in itertools.combinations(columns, 2):
+
+            SPvalues, Pvalues = stats.spearmanr(d_pivot.loc[:, col_a], d_pivot.loc[:, col_b])
+            newIndex = col_a + '_' + col_b
+
+            newCorrLine = pd.DataFrame.from_dict({'index': [newIndex], 'SPvalues': [SPvalues], 'Pvalues': [Pvalues], 'AB1': [col_a], 'AB2': [col_b]}, orient='columns')
+            correlations = pd.concat([correlations, newCorrLine], ignore_index = True)
+            
+        return(correlations)
+    
+    def __calculate_all_spearman_correlations_per_cluster(self, data):
+        
+        dataGroundtruth = self.data["Groundtruth"].copy()
+        correlationsPerCluster = {}
+        for cluster in dataGroundtruth['cluster_id'].unique():
+            data = data[data["cluster_id"] == cluster]
+
             data_subset = data.loc[:,['sample_id','ab_id', 'ab_count_normalized']]
             d_pivot=data_subset.pivot(index = "sample_id", columns='ab_id', values='ab_count_normalized')
             columns = d_pivot.columns.tolist()
@@ -369,11 +400,13 @@ class NormalizedDataHandler:
 
                 newCorrLine = pd.DataFrame.from_dict({'index': [newIndex], 'SPvalues': [SPvalues], 'Pvalues': [Pvalues], 'AB1': [col_a], 'AB2': [col_b]}, orient='columns')
                 correlations = pd.concat([correlations, newCorrLine], ignore_index = True)
-            return(correlations)
+                
+            correlationsPerCluster[cluster] = correlations
+        return(correlationsPerCluster)
 
-    def save_all_correlations(self, data, key):
+    def save_all_correlations(self, data, key, cluster):
         for row in data.itertuples():
-            self.abCorr.write(key + "\t" + row.AB1 + "\t" + row.AB2 + "\t" + str(round(row.SPvalues, 4)) + "\t" + str(round(row.SPvalues_norm, 4)) + "\n")
+            self.abCorr.write(cluster + "\t" + key + "\t" + row.AB1 + "\t" + row.AB2 + "\t" + str(round(row.SPvalues, 4)) + "\t" + str(round(row.SPvalues_norm, 4)) + "\n")
 
     def ab_spearman_correlation_graph(self, filter = ""):
         plt.figure(figsize=(16,10))
@@ -385,7 +418,7 @@ class NormalizedDataHandler:
             #speacial line for a data set where phospho proteins were excluded due to expected correlations
             if(filter != ""):
                 data = data[data['ab_type'] == filter]
-            spearmanValues = self.__calculate_all_spearman_correlations(data)
+            spearmanValues = self.__calculate_all_spearman_correlations(data.copy())
             sp_mean = np.mean(spearmanValues.SPvalues)
             p_mean = np.mean(spearmanValues.Pvalues)
 
@@ -394,39 +427,58 @@ class NormalizedDataHandler:
             lengend_labels.append(key)
 
         plt.legend(labels=lengend_labels)
-        plt.savefig(self.folder_path +  "SpearmanCorrelations/spearman_correlations_" + filter + ".png", dpi=199)
+        plt.savefig(self.folder_path +  "SpearmanCorrelations/spearman_correlations_global_" + filter + ".png", dpi=199)
         plt.close()
+        
+    def calculate_all_correlations(self):
+        
+        for key in self.data:
+            dataAllClusters = self.data[key].copy()
+            column_names = ["cluster","index", "SPvalues", "Pvalues", "AB1", "AB2"]
+            correlations = pd.DataFrame(columns = column_names)
+            for cluster in dataAllClusters['cluster_id'].unique():
+                data = dataAllClusters[dataAllClusters["cluster_id"] == cluster]
+                correlationsTmp = self.__calculate_all_spearman_correlations(data.copy())
+                correlationsTmp["cluster"] = cluster
+                correlations = pd.concat([correlations, correlationsTmp], ignore_index=True)
 
+            self.correlations[key] = correlations
+            
     def ab_correlation_evaluation(self):
 
         #build a dataFrame that calculates all correlations between proteins for grondTruth
-        groundTruthCorrelations = self.__calculate_all_spearman_correlations(self.groundtruth.copy())
+        groundTruthCorrelations = self.correlations["Groundtruth"]
         
         normRMSDData = {}
         #for every norm method
         for key in self.data:
             if("Groundtruth" in key):
                 continue
+            #get cluster specific correlations
+            dataAllClusters = self.data[key].copy()
+            for cluster in dataAllClusters['cluster_id'].unique():
 
-            data = self.data[key].copy()
-            #calculate same dataFrame and add GroundTruth to it
-            normCorrelations = self.__calculate_all_spearman_correlations(data)
-            normCorrelations.columns = ['index','SPvalues_norm', 'Pvalues_norm', 'AB1_norm', 'AB2_norm']
+                #calculate same dataFrame and add GroundTruth to it
+                normCorrelations = self.correlations[key]
+                normCorrelations = normCorrelations[normCorrelations["cluster"] == cluster]
+                normCorrelations.columns = ["cluster", "index","SPvalues_norm", "Pvalues_norm", "AB1_norm", "AB2_norm"]
+                
+                groundTruthCorrelationsTmp = groundTruthCorrelations[groundTruthCorrelations["cluster"] == cluster]
+                
+                #dropping nan values, important for e.g. subsampling
+                groundTruthCorrelationsTmp.dropna(subset = ["SPvalues"], inplace=True)
+                normCorrelations.dropna(subset = ["SPvalues_norm"], inplace=True)
 
-            #dropping nan values, important for e.g. subsampling
-            groundTruthCorrelations.dropna(subset = ["SPvalues"], inplace=True)
-            normCorrelations.dropna(subset = ["SPvalues_norm"], inplace=True)
+                result = pd.merge(groundTruthCorrelationsTmp, normCorrelations, how="inner", on = "index")
 
-            result = pd.merge(groundTruthCorrelations, normCorrelations, how="inner", on = "index")
+                #calculate the RMSD and safe it to RMSD matrix
+                mse = sklearn.metrics.mean_squared_error(result.SPvalues, result.SPvalues_norm)
+                rmsd = math.sqrt(mse)
+                normRMSDData[key] = rmsd
+                self.rmsd_sp_results.write(cluster + "\t" + key + "\t"+ str(round(rmsd,4)) + "\n")
 
-            #calculate the RMSD and safe it to RMSD matrix
-            mse = sklearn.metrics.mean_squared_error(result.SPvalues, result.SPvalues_norm)
-            rmsd = math.sqrt(mse)
-            normRMSDData[key] = rmsd
-            self.rmsd_sp_results.write(key + "\t"+ str(round(rmsd,4)) + "\n")
-
-            #wite all correlations to file for AB pairs
-            self.save_all_correlations(result, key)
+                #wite all correlations to file for AB pairs
+                self.save_all_correlations(result, key, cluster)
 
         #draw barplot of all RMSDs
         values = normRMSDData.values()
@@ -507,120 +559,58 @@ class NormalizedDataHandler:
         plt.savefig(self.folder_path + "Overview/ABCountRMSD.png", dpi=199)
         plt.close()
 
-    def validate_correlations(self, proteinCorrelations):
+    def validate_correlations(self, corrCutoff = 0.5, stepSize = 0.05):
         
-        #for the proteinCorrelations calcualte the spearman correlation for groundtruth and norm
-        indices = []
-        columnNames = ["Groundtruth"]
-        wantedVarProteinList = []
-        for corr in proteinCorrelations:
-            prot1 = corr.prot1
-            prot2 = corr.prot2
-            wantedVarProteinList.append(prot1)
-            wantedVarProteinList.append(prot2)
-            indices.append(prot1 + '_' + prot2)
+        #get list of all indeces (AB1_AB2) of TP correlations
+        truePosCorrelations = []
+        column_names = ["cluster", "norm", "minCorr", "TP", "TN", "FP", "FN"]
+        correlationRocResults = pd.DataFrame(columns = column_names)
+        groundTruthCorrelations = self.correlations["Groundtruth"].copy()
+        
+        groundtruthData = self.data["Groundtruth"].copy()
+        clusters = groundtruthData['cluster_id'].unique()
+        
+        for cluster in clusters:
+            groundTruthTmp = groundTruthCorrelations[groundTruthCorrelations["cluster"] == cluster]
+            for index, row in groundTruthTmp.iterrows():
+                if(row["SPvalues"] > corrCutoff):
+                    truePosCorrelations.append(row["index"])
+                    
+        #for all norm methods        
         for key in self.data:
             if("Groundtruth" in key):
                 continue
-            columnNames.append(key)
-        correlations = pd.DataFrame(columns = columnNames, index  = indices)
+            
+            #for all clusters
+            for cluster in clusters:
+                normData = self.correlations[key].copy()
+                normData = normData[normData["cluster"] == cluster]
+                
+                cutOffTmp = 1.0
+                while 0.0 <= cutOffTmp:
+                    
+                    #get correlastions > threshold
+                    detectedCorrelations = []
+                    for index, row in normData.iterrows():
+                        if(row["SPvalues"] > cutOffTmp):
+                            detectedCorrelations.append(row["index"])
+                           
+                    #count number of TP,TN,FP,FN indices and add to result
+                    tp = sum(corr in truePosCorrelations for corr in detectedCorrelations)
+                    fp = len(detectedCorrelations) - tp
+                    
+                    tn = len(normData.index) - len(truePosCorrelations) - fp
+                    fn = len(normData.index) - tn - len(detectedCorrelations)
+                            
+                    data = {"cluster": [cluster],"norm": [key], "minCorr": [cutOffTmp],
+                            "TP":[tp], "TN":[tn], "FP":[fp], "FN":[fn]}
+                    resultTmp = pd.DataFrame(data)
+                    correlationRocResults = pd.concat([correlationRocResults, resultTmp], ignore_index=True)
 
-        for corr in proteinCorrelations:
-            prot1 = corr.prot1
-            prot2 = corr.prot2
-            #groundtruth
-            dataTruth = self.groundtruth.copy()
-            dataTruth = dataTruth[['sample_id', 'ab_id', 'ab_count_normalized']]
-            spvalue = stats.spearmanr(dataTruth.loc[dataTruth.ab_id == prot1, ["ab_count_normalized"]], dataTruth.loc[dataTruth.ab_id == prot2, ["ab_count_normalized"]]).correlation
-            correlations.loc[prot1 + '_' + prot2, "Groundtruth"] = float(spvalue)
+                    cutOffTmp = cutOffTmp - stepSize
+                    self.corrDetection.write(cluster + "\t" + key + "\t"+ str(round(cutOffTmp, 2))+ "\t" + str(tp) + "\t" + str(tn) + "\t" + str(fp) + "\t" + str(fn) + "\n")
 
-            for key in self.data:
-                if("Groundtruth" in key):
-                    continue
-                data = self.data[key].copy()
-                data = data[['sample_id', 'ab_id', 'ab_count_normalized']]
-                spvalue = stats.spearmanr(data.loc[data.ab_id == prot1, ["ab_count_normalized"]], data.loc[data.ab_id == prot2, ["ab_count_normalized"]]).correlation
-                correlations.loc[prot1 + '_' + prot2, key] = float(spvalue)
-
-        #correlations.astype(np.float64)
-        cols = correlations.columns.values.tolist()
-        correlations[cols] = correlations[cols].apply(pd.to_numeric, errors='coerce', axis=1)
-        correlations.round(2)
-        wantedCorrelations = correlations.copy()
-
-        #ax= plt.subplot()
-        #sns.heatmap(correlations, annot=True, annot_kws={"size": 5}, cmap="viridis", vmin = 0, vmax = 1) # font size
-        #ax.set_ylabel('AB Correlations', fontsize = 10)
-        #ax.set_title('AB Correlations', fontsize = 10)
-        #ax.vlines([1], *ax.get_ylim(), linewidth = 2, color = "red")
-        #plt.xticks(rotation = 45, ha='right', rotation_mode='anchor', fontsize = 8)
-        #plt.yticks(rotation = 45, ha='right', rotation_mode='anchor', fontsize = 8)
-        #plt.legend( fontsize='8', title_fontsize = '10') # for legend text
-        #plt.tight_layout()
-        #plt.savefig(self.folder_path + "Overview/WantedCorrelationHeatmap.png", dpi=199)
-        #plt.close()
-
-        #UNWANTED CORRELATIONS
-        #for the proteinCorrelations calcualte the spearman correlation for groundtruth and norm
-        unwantedProteins = self.groundtruth["ab_id"].unique()
-        unwantedProteinList = [x for x in unwantedProteins if x not in wantedVarProteinList]
-
-        negProteinCorrelations = []
-        for prot1, prot2 in itertools.combinations(unwantedProteinList, 2):
-            cor = (prot1, prot2)
-            negProteinCorrelations.append(cor)
-
-        sampleNumCorrelations = min(50, len(negProteinCorrelations))
-        negProteinCorrelations = random.sample(negProteinCorrelations, sampleNumCorrelations)
-        indices = []
-        columnNames = ["Groundtruth"]
-        for corr in negProteinCorrelations:
-            prot1 = corr[0]
-            prot2 = corr[1]
-            indices.append(prot1 + '_' + prot2)
-        for key in self.data:
-            if("Groundtruth" in key):
-                continue
-            columnNames.append(key)
-        correlations = pd.DataFrame(columns = columnNames, index  = indices)
-
-        for corr in negProteinCorrelations:
-            prot1 = corr[0]
-            prot2 = corr[1]
-            #groundtruth
-            dataTruth = self.groundtruth.copy()
-            dataTruth = dataTruth[['sample_id', 'ab_id', 'ab_count_normalized']]
-            spvalue = stats.spearmanr(dataTruth.loc[dataTruth.ab_id == prot1, ["ab_count_normalized"]], dataTruth.loc[dataTruth.ab_id == prot2, ["ab_count_normalized"]]).correlation
-            correlations.loc[prot1 + '_' + prot2, "Groundtruth"] = float(spvalue)
-
-            for key in self.data:
-                if("Groundtruth" in key):
-                    continue
-                data = self.data[key].copy()
-                data = data[['sample_id', 'ab_id', 'ab_count_normalized']]
-                spvalue = stats.spearmanr(data.loc[data.ab_id == prot1, ["ab_count_normalized"]], data.loc[data.ab_id == prot2, ["ab_count_normalized"]]).correlation
-                correlations.loc[prot1 + '_' + prot2, key] = float(spvalue)
-
-        #correlations.astype(np.float64)
-        cols = correlations.columns.values.tolist()
-        correlations[cols] = correlations[cols].apply(pd.to_numeric, errors='coerce', axis=1)
-        correlations.round(2)
-
-        correlations = pd.concat([wantedCorrelations, correlations])
-
-        ax= plt.subplot()
-        sns.heatmap(correlations, annot=False, annot_kws={"size": 5}, cmap="bwr", vmin = -1, vmax = 1, cbar_kws={"shrink": .70}) # font size
-        # labels, title and ticks
-        ax.set_ylabel('AB Correlations', fontsize = 10)
-        ax.set_title('AB Correlations', fontsize = 10)
-        ax.vlines([1], *ax.get_ylim(), linewidth = 2, color = "red")
-        ax.hlines([len(wantedCorrelations)], *ax.get_xlim(), linewidth = 2, color = "red")
-        plt.xticks(rotation = 45, ha='right', rotation_mode='anchor', fontsize = 8)
-        plt.yticks(rotation = 45, ha='right', rotation_mode='anchor', fontsize = 6)
-        plt.tight_layout()
-        plt.savefig(self.folder_path + "Overview/UnwantedCorrelationHeatmap.png", dpi=199)
-        plt.close()
-
+        print(correlationRocResults)
     #mean values are in the end divided by the smallest AB value
     def __get_AB_mean_dataFrame(self, data):
 
@@ -758,19 +748,28 @@ class NormalizedDataHandler:
 
         #for every norm method
         for key in self.data:
+
             if("Groundtruth" in key):
                 continue
-            data = self.data[key].copy()
+            
+            dataAllClusters = self.data[key].copy()
+            for cluster in dataAllClusters['cluster_id'].unique():
+                data = dataAllClusters[dataAllClusters["cluster_id"] == cluster]
+                
+                groundtruth = self.groundtruth.copy()
+                groundtruth = groundtruth[groundtruth["cluster_id"] == cluster]
+                overlapDist = calculate_knn_overlap(data, groundtruth)
+                overlapDist['NORM_METHOD'] = str(key)
+                overlapDist['CLUSTER_ID'] = str(cluster)
 
-            overlapDist = calculate_knn_overlap(data.copy(), self.groundtruth.copy())
-            overlapDist['NORM_METHOD'] = str(key)
-
-            if(result is None):
-                result = overlapDist
-            else:
-                result = pd.concat([result, overlapDist], ignore_index=True)
+                if(result is None):
+                    result = overlapDist
+                else:
+                    result = pd.concat([result, overlapDist], ignore_index=True)
 
         #write final results
+        print("KNN GRAPH")
+        print(result)
         result.to_csv(self.knnOverlapFilePath, sep='\t', index=False)
 
 

src/simulation/ABseqSimulation/Simulation.py
@@ -46,15 +46,24 @@ def replace_negatives(x):
 #introduce a technical variance matrix: AB amount differences, annealing differences
 
 def convert_neg_binom_params(mu, size):
-    p = size/(size+mu)
-    n = mu*p/(1.0 - p)
+    #n = number of successes/ p = probability
+    #p = size/(size+mu) - same as formular below
+    # formular for probability here: https://stat.ethz.ch/R-manual/R-devel/library/stats/html/NegBinomial.html    
+    #in nbinom (python) n is number of successes, in R this is already size, n is the number of observations
+    
+    n = size
+    p = 1 / (1 + (mu / size))
+
     return n, p
     
 class ProteinCountDistribution():
 
     def __convert_params(self, mu, size):
-        p = size/(size+mu)
-        n = mu*p/(1.0 - p)
+        #p = size/(size+mu)
+        
+        n = size
+        p = 1 / (1 + (mu / size))
+    
         return n, p
 
     def __init__(self, number, mu, size):
@@ -73,6 +82,9 @@ class Parameters():
     #antibody count is not considered for now: in sc IDseq constant AB concentration was used for all targets (0.1myg/ml over night)
     #biological variation matrix
     #technical variation matrix
+    
+    #store its own file name (needed to write benchmarkIniFile)
+    filePath = ""
     
     #data types to hold proteinlevels/dist/correlations which are inserted in dictionaries mapping them to individual proteins later on
     ProteinMeanDist = namedtuple('ProteinLevel', ['mu', 'size', 'number'])
@@ -93,7 +105,7 @@ class Parameters():
     abDuplicateDisturbance=0
     pcrCycles=1
     pcrCapture=1
-    libSize=[1,1]
+    libSize=[1.0,0.0] #default lib-size factor is 1.0 introducing NO lib-size effect
     batchFactors=None
     noise=0.0
     noiseExtrinsic=0.0
@@ -320,8 +332,13 @@ class Parameters():
             
     def __init__(self, paramter_file):
         self.__parseParameters(paramter_file)
+        self.filePath = paramter_file
 
 class SingleCellSimulation():
+
+    #certain variables drawn from distributions (correlations) have to be stored to be accessed in benchmark
+    """ BenchmarkIniFiles """
+    benchmarkIniPath = ""     
 
     """ PARAMETERS """
     parameters = None
@@ -337,7 +354,49 @@ class SingleCellSimulation():
 
     def __init__(self, parameters):
         self.parameters = parameters
+        indexSlash = self.parameters.filePath.rfind("/")
+        self.benchmarkIniPath = self.parameters.filePath[:indexSlash + 1] + "benchmarkIniFile" + self.parameters.filePath[indexSlash + 1:]
 
+    #e.g., to write the proteins involved in clusters to benchmarkIniFile
+    def __write_cluster_proteins(self, file_path, data):
+        """
+        Write a list to a file.
+        :param file_path: The path to the file.
+        :param data: The list of lists: protein list per cluster
+        """
+        try:
+            # Open the file in write mode
+            with open(file_path, 'w') as file:
+                # Write each element of the list to the file
+                for idx in range(self.parameters.numberOfClusters-1):
+                    file.write(str(idx) + ':\t')
+                    for protein in data[idx]:
+                        file.write(str(protein) + '\t')
+                    file.write('\n')
+            print("Data written to", file_path)
+        except Exception as e:
+            print("Error writing to file:", str(e))
+            
+    #e.g., to write the proteinCorrelations to benchmarkIniFile (witha  cutoff stated in main)
+    def __write_correlated_proteins(self, file_path, covariancematrix):
+        """
+        Write a list to a file.
+        :param file_path: The path to the file.
+        :param data: The list to write to the file.
+        """
+        try:
+            # Open the file in write mode
+            with open(file_path, 'w') as file:
+                # Write each element of the list to the file
+                print(file_path)
+                for item in data:
+                    print(item)
+
+                    file.write(str(item) + '\n')
+            print("Data written to", file_path)
+        except Exception as e:
+            print("Error writing to file:", str(e))
+            
     """ we see a strong dependance of size on mu for single-cell protein distributions, therefore calualte size depending on mu"""
     def __calculate_size_from_mu(self, mu):
         size_formula_with_mu_insertion = self.parameters.size.replace('MU', str(mu))
@@ -462,7 +521,7 @@ class SingleCellSimulation():
         perturbFrame = pd.DataFrame()
         perturbFrame["sample_id"] = data["sample_id"].unique()
         #sample lbisize factors from normal dist: 1.value = mean, 2.= std
-        perturbFrame["factor"] = np.random.lognormal(self.parameters.libSize[0],self.parameters.libSize[1], len(perturbFrame))
+        perturbFrame["factor"] = np.random.normal(self.parameters.libSize[0],self.parameters.libSize[1], len(perturbFrame))
 
         perturbDict = dict(zip(perturbFrame["sample_id"], perturbFrame["factor"]))
         data["ab_count"] = data["ab_count"] * data["sample_id"].map(perturbDict)
@@ -535,9 +594,6 @@ class SingleCellSimulation():
         if(setIdx is None):
             mean = self.parameters.proteinCorrelationMean * scalingFactor
             std = self.parameters.proteinCorrelationStd
-            print(self.parameters.proteinCorrelationStd)
-            print(str(mean))
-
         else:
             mean = self.parameters.proteinCorrelationDists[setIdx].mean * scalingFactor
             std = self.parameters.proteinCorrelationDists[setIdx].std
@@ -623,13 +679,9 @@ class SingleCellSimulation():
                     covMat[j][i] = corr
                     
         #check if matrix is positive semi definite
-        print("___BEORE___\n")
-        print(np.round(covMat,2))
         minEigenVal = min(np.linalg.eigvals(covMat))
         if(minEigenVal <0):
             covMat = nearcorr(np.array(covMat), max_iterations=10000)
-        print("___AFTER___\n")
-        print(np.round(covMat,2))
 
         return(covMat)
         
@@ -669,9 +721,6 @@ class SingleCellSimulation():
         return(distributions)
     
     def __sampleCellTimesProteinMatrix(self, percentage, dist, cov, clusterIdx):
-
-        print(np.round(cov,2))
-        print(dist)
         
         copula_params = {}
         copula_params['correlation'] = cov
@@ -685,14 +734,23 @@ class SingleCellSimulation():
 
         #bring copulaResult in the right format
         copulaResult = copulaResult.reset_index().rename(columns={ 'index' : 'sample_id'})
+        infNum = np.isinf(copulaResult).values.sum()
+        if(infNum > 0):
+            print("simulated data contains " + infNum + "INF values: check simulations! (e.g., wrong correlation parameters\n)")
+            exit()
+            
         copulaResult = copulaResult.melt(id_vars = ["sample_id"], var_name="ab_id", value_name="ab_count")
         copulaResult.insert(0, 'batch_id', 'batch')
         copulaResult.insert(0, 'ab_type', 'any_ab')
 
+        #insert treatment/cluster column +
+        #add cluster prefix to sample_id (since we have the same idx for all clusters)
         if(clusterIdx == 0):
             copulaResult.insert(0, 'cluster_id', 'control')
+            copulaResult['sample_id'] = 'control_' + copulaResult['sample_id'].astype(str)
         else:
             copulaResult.insert(0, 'cluster_id', ('cluster_' + str(clusterIdx)))
+            copulaResult['sample_id'] = 'cluster_' + str(clusterIdx) + "_" + copulaResult['sample_id'].astype(str)
 
         return(copulaResult)
     
@@ -702,8 +760,10 @@ class SingleCellSimulation():
         
         betaFactors = self.parameters.betaFactors
         betaFactors.insert(0, 1.0)
+        betaParams = [self.parameters.betaParameter * x for x in betaFactors]
+        assert(len(betaParams) == self.parameters.numberOfClusters)
         for idx in range(self.parameters.numberOfClusters):
-            covariancematrix.append(self.__vineBeta(self.parameters.ProteinNumber, betaFactors[idx]))
+            covariancematrix.append(self.__vineBeta(self.parameters.ProteinNumber, betaParams[idx]))
 
         return(covariancematrix)
     
@@ -756,7 +816,7 @@ class SingleCellSimulation():
             for idx in range(self.parameters.numberOfClusters-1):
                 subset = random.sample(range(self.parameters.ProteinNumber), self.parameters.numberClusterSpecificProteins[idx])
                 clusterSpecificProteinIdxs.append(subset)
-                
+                              
         #CLUSTER SPECIFIC PROTEIN-DISTRIBUTION-PARAMETRIZATION (scaled for proteins)
         for idx in range(self.parameters.numberOfClusters):
             distributions.append(self.__generateProteinDistributions(clusterSpecificProteinIdxs, idx))
@@ -971,7 +1031,7 @@ class SingleCellSimulation():
         #for every cluster: scale protein distributions; scale correlations; sample using gaussian copula
         # (protein counts scaled/ correlations scaled/ and then sampled from theirs dists by copula)
         self.groundTruthData = self.__generateGroundTruthWithCopula()
-
+            
         #not interesting at this point (initialy thought of AB duplicates for normalization)
         if(self.parameters.abDuplicates > 1):
             self.groundTruthData = self.__insert_ab_duplicates(self.groundTruthData)
@@ -982,6 +1042,9 @@ class SingleCellSimulation():
             perturbedData = self.__insert_batch_effect(perturbedData)
             self.__add_batch_effect_to_ground_truth(perturbedData)
             
+        #add random noise to simulations
+        tmp_simulatedData = self.__perturb(perturbedData)
+        
         #scale counts for library size effects
         if(not (self.parameters.libSize[0]==1 and self.parameters.libSize[1]==1)):
             perturbedData = self.__insert_libsize_effect(perturbedData)
@@ -993,9 +1056,6 @@ class SingleCellSimulation():
         #simulate PCR amplification and sequencing
         #sampling with replacement to simulate PCR amplification as well as missing out on reads during washing/ sequencing
         #tmp_simulatedData = self.__simulate_sequencing_binding_2(perturbedData)
-        
-        #add random noise to simulations
-        tmp_simulatedData = self.__perturb(perturbedData)
 
         #remove negative counts from data, and substitute with zero
         self.simulatedData = tmp_simulatedData
@@ -1015,4 +1075,3 @@ class SingleCellSimulation():
         self.simulatedData.to_csv(self.output_dir + "/" + self.parameters.simulationName + "_SIMULATED.tsv", sep='\t', index = False)
         self.groundTruthData.to_csv(self.output_dir + "/" + self.parameters.simulationName + "_GROUNDTRUTH.tsv", sep='\t', index = False)
         printToTerminalOnce("\tData saved\n")
-

src/simulation/ABseqSimulationBenchmark/main.py
@@ -42,7 +42,6 @@ def parse_args():
     parser.add_argument('--d', help='duplicates',
                         type=int, default=5)
 
-
     args = parser.parse_args()
     
     return(args)
@@ -85,6 +84,7 @@ def generate_simulation_iniFiles(iniFile, fileBenchmarksToKeep):
     while line:
         if( (not line.startswith("#")) and ("INIRANGE" in line) ):
             info = re.match(("(.+?)INIRANGE=(.*)"), line)
+
             variableParameter = str(info[1]) 
             infoArray = str(info[2]).split(";")
             #if the range itself has several values (so far only 2) parse them seperately
@@ -123,6 +123,10 @@ def generate_simulation_iniFiles(iniFile, fileBenchmarksToKeep):
     count = 0
     fileBenchmarksToKeep.append(0)
 
+    #if we have no INIRANGE: write at least the file itself by setting end to 2
+    #to initiate a single iteration
+    if(start == 1 and end == 1 and factor == 1):
+        end = 2
     for i in np.arange(start, end, factor):
         j = 0
         if value2Set:
@@ -135,10 +139,10 @@ def generate_simulation_iniFiles(iniFile, fileBenchmarksToKeep):
         file = open(iniFile, "r")
         line = file.readline()
         while line:
-            if(str.startswith(line, variableParameter+"INIRANGE")):
+            if(str.startswith(line, variableParameter+"INIRANGE") and (variableParameter != "")):
                 line = file.readline()
                 continue
-            elif(str.startswith(line, variableParameter)):
+            elif(str.startswith(line, variableParameter) and (variableParameter != "")):
                 #write new variable line
 
                 #LIBRARY SIZE
@@ -183,6 +187,12 @@ def generate_simulation_iniFiles(iniFile, fileBenchmarksToKeep):
                 if(variableParameter=="correlationFactors"):
                     newLine = line.replace("X", str(i))
                     newFile.write(newLine)
+                if(variableParameter=="betaParameter"):
+                    newLine = line.replace("X", str(i))
+                    newFile.write(newLine)  
+                if(variableParameter=="betaFactors"):
+                    newLine = line.replace("X", str(i))
+                    newFile.write(newLine)  
                 #increasing cellPercentages (have in mind, all NON-X cell percentages get evenly scaled to sum to 100)
                 if(variableParameter=="cellPercentages"):
                     #we need to scale other cell population percentages so that they sum to 100